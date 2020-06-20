953ae4e33b110ea527c6b5807739212beaeef8c3
Michał <michal_duda@onet.pl>
2020-06-20 20:58:13 +0200

    Uzupenienie klas asocjacyjnych

--- Kolokwium1/Model/Champiosnship_Team.cs
+++ Kolokwium1/Model/Champiosnship_Team.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,14 @@ namespace Kolokwium1.Model
 {
     public class Champiosnship_Team
     {
-        public float score { get; set; }
+        [Key]
+        [ForeignKey("Team")]
+        public int? IdTeam { get; set; }
+
+        [Key]
+        [ForeignKey("Champiosnship")]
+        public int? IdChampiosnship { get; set; }
+
+        public float Score { get; set; }
     }
 }
--- Kolokwium1/Model/Player_Team.cs
+++ Kolokwium1/Model/Player_Team.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,18 @@ namespace Kolokwium1.Model
 {
     public class Player_Team
     {
+        [Key]
+        [ForeignKey("Team")]
+        public int? IdTeam { get; set; }
+        
+        [Key]
+        [ForeignKey("Player")]
+        public int? IdPlayer { get; set; }
+
+        [MaxLength(300)]
+        [Required]
+        public string Comment { get; set; }
+
+        public int NumOnShirt { get; set; }
     }
 }
